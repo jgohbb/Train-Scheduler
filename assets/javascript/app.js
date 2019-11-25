119e90069429be028a043f03c093d5eaa68e02e7
jgohbb <55856950+jgohbb@users.noreply.github.com>
2019-11-25 00:07:01 -0700

    first stage in progress started on framework and script

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyB_3Q05srirYFcRWbJYH886lCYB1q5G4U4",
+  authDomain: "bootcamp-train.firebaseapp.com",
+  databaseURL: "https://bootcamp-train.firebaseio.com",
+  projectId: "bootcamp-train",
+  storageBucket: "bootcamp-train.appspot.com",
+  messagingSenderId: "857668367385",
+  appId: "1:857668367385:web:ab5f0285d682d6649aa688",
+  measurementId: "G-08SYXY66MJ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
